e33d71681a55cc9e9e4a5d7f51c5cecde7873d02
chunngamm <kjwharry@naver.com>
2022-10-17 19:35:55 +0900

    post커밋함

---
+++ src/main/java/com/example/sa_advanced/controller/request/PostRequestDto.java
@@ -0,0 +1,16 @@
+package com.example.sa_advanced.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostRequestDto {
+    private String title;
+    private String content;
+}
